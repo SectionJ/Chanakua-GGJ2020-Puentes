0cbaa40237d0cde1c51703cde250714b6ee5eb52
Jordan <jordanwesker@hotmail.com>
2020-02-01 22:24:17 -0600

    ajuste de pantallas iniciales

---
+++ CGGJ-Puentes/Assets/Jordan/Script/Perica/ScreenOrganize.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenOrganize : MonoBehaviour
+{
+    public int NumberOfPlayers;
+    public Canvas [] ScreensForPlayers;
+    // Start is called before the first frame update
+    void Start(){
+
+        //dependiendo del numero de jugadores se activara el 
+        //canvas necesario para poder mostrar la UI
+        switch (NumberOfPlayers){
+            case 2:
+                ScreensForPlayers[0].enabled = true;
+                break;
+
+            case 3:
+                ScreensForPlayers[1].enabled = true;
+                break;
+            
+            case 4:
+                ScreensForPlayers[2].enabled = true;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){
+    }
+}
