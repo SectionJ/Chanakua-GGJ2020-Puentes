1b6b0bc7d181d435e79a7a4b00b03b27cf5a3984
Jordan <jordanwesker@hotmail.com>
2020-02-02 12:26:58 -0600

    Pantalla de seleccion

---
+++ CGGJ-Puentes/Assets/Jordan/Script/ScreenSelector/SelectorController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SelectorController : MonoBehaviour{
+
+    public GameObject fadeing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("FadeCustom");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CaracterSelection(){
+        fadeing.SetActive(true);
+        fadeing.GetComponent<Animator>().SetTrigger("FadeOut");
+        StartCoroutine("Fadde");
+    }
+    
+    IEnumerator FadeCustom(){
+        yield return new WaitForSeconds(1);
+        fadeing.SetActive(false);
+    }
+
+    IEnumerator Fadde(){
+        yield return new WaitForSeconds(2);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Perica");
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone){
+            yield return null;
+        }
+    }
+}
